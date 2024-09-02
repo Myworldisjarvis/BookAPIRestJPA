34628497f700c44523b43fd0cdbcc4034a6f5cf7
Myworldisjarvis <namesadaf@gmail.com>
2024-09-02 20:36:45 +0530

    final book api with service and status done

--- bootrestbook/src/main/java/com/restapi/book/services/BookService.java
+++ bootrestbook/src/main/java/com/restapi/book/services/BookService.java
@@ -5,31 +5,45 @@ import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.restapi.book.dao.BookRepository;
 import com.restapi.book.entites.Book;
 
 //@service best hai ya fir aap @component use kar sakte hai
 @Service
 public class BookService {
 
-	private static List<Book> booklist = new ArrayList<>();
-
-	static {
-		booklist.add(new Book(2, "python", "patani"));
-		booklist.add(new Book(3, "javascrip", "patani"));
-	}
+	@Autowired
+	private BookRepository bookRepository;
+	
+	
+	
+//	private static List<Book> booklist = new ArrayList<>();
+//
+//	static {
+//		booklist.add(new Book(2, "python", "patani"));
+//		booklist.add(new Book(3, "javascrip", "patani"));
+//	}
 
 	// get all books
 	public List<Book> getAllBooks() {
-		return booklist;
+		
+		List<Book> list =  (List<Book>) this.bookRepository.findAll();
+		
+		return list;
 	}
 
 	// get book by id
 	public Book getBookById(int id) {
 		Book book = null;
 		try {
-			book = booklist.stream().filter(e -> e.getBookId() == id).findFirst().get();	
+//			book = booklist.stream().filter(e -> e.getBookId() == id).findFirst().get();	
+			
+			 book  = this.bookRepository.findById(id);
+			
+			
 		} catch (Exception e2) {
 			e2.printStackTrace();
 		}
@@ -40,27 +54,33 @@ public class BookService {
 
 	// ADD BOOK USING CLIENT
 	public Book addBook(Book b) {
-		booklist.add(b);
+			Book book = this.bookRepository.save(b);
 
-		return b;
+		return book;
 	}
 
 	// delete book by id
 	public void deleteBook(int id) {
 
+		
+		this.bookRepository.deleteById(id);
+		
 //		for (Book book : booklist) {
 //			if(book.getBookId()==id) {
 //				booklist.remove(book); 
 //			}
 //		}
 
-		booklist = booklist.stream().filter(book -> book.getBookId() != id).collect(Collectors.toList());
+//		booklist = booklist.stream().filter(book -> book.getBookId() != id).collect(Collectors.toList());
 
 	}
 
 	// update book by id
 	public void updateBook(Book b, int id) {
-
+		
+		b.setBookId(id);
+		this.bookRepository.save(b);
+		 
 //		for (Book book : booklist) {
 //			if(book.getBookId()==id) {
 //				 // Update the book details
@@ -72,15 +92,15 @@ public class BookService {
 //		}
 //		return null;
 
-		booklist = booklist.stream().map(book -> {
-
-			if (book.getBookId() == id) {
-				book.setBookId(b.getBookId());
-				book.setTitle(b.getTitle());
-				book.setAuthor(b.getAuthor());
-				}
-			return book;
-		}).collect(Collectors.toList());
+//		booklist = booklist.stream().map(book -> {
+//
+//			if (book.getBookId() == id) {
+//				book.setBookId(b.getBookId());
+//				book.setTitle(b.getTitle());
+//				book.setAuthor(b.getAuthor());
+//				}
+//			return book;
+//		}).collect(Collectors.toList());
 
 	}
 
